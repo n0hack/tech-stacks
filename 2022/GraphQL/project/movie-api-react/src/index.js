c08164f0bae7e7e17398c37671707e76b98b21d8
n0hack <nohack-@naver.com>
2022-05-02 17:42:23 +0900

    [feat] apollo-client 적용

--- 2022/GraphQL/project/movie-api-react/src/index.js
+++ 2022/GraphQL/project/movie-api-react/src/index.js
@@ -1,10 +1,14 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import client from './apollo';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
