5d40ead701b9a67a97707b07876014818bb4f3a6
n0hack <nohack-@naver.com>
2022-05-04 07:51:55 +0900

    [syntax] mapped type - handbook

---
+++ 2022/TypeScript/docs/build/mapped.js
@@ -0,0 +1,12 @@
+const heroInfo = {
+    Hulk: 54,
+    Thor: 1000,
+    Capt: 33,
+};
+// 부분 집합
+const ageOnly = { age: 23 };
+const nameOnly = { name: 'NoHack' };
+const emptyPerson = {};
+// @ts-ignore
+function fetchUserProfile() { }
+function updateUserProfile(params) { }
