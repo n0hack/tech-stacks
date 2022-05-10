8dc777ad2eb42e7832aaa230cef51fc4f82fcb5e
n0hack <nohack-@naver.com>
2022-05-10 21:13:01 +0900

    context api

--- 2022/React/docs/src/App.js
+++ 2022/React/docs/src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import FilterableProductTable from './components/FilterableProductTable';
+import React, { createContext, useContext } from 'react';
+import { ThemeContext } from './context/theme';
+
+const ThemedButton = (props) => {
+  const theme = useContext(ThemeContext);
+  console.log(theme);
+  return (
+    <button {...props} style={{ backgroundColor: theme.background }}>
+      반갑고
+    </button>
+  );
+};
 
 const App = () => {
-  return <FilterableProductTable />;
+  return (
+    <>
+      <section>
+        <ThemedButton />
+      </section>
+    </>
+  );
 };
 
 export default App;
