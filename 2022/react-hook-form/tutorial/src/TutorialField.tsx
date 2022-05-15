d95e57d123771782dc53d0db215f449aca5b3f89
n0hack <nohack-@naver.com>
2022-05-16 08:31:12 +0900

    [시작하기] 필드 등록하기

---
+++ 2022/react-hook-form/tutorial/src/TutorialField.tsx
@@ -0,0 +1,33 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+enum GenderEnum {
+  female = "female",
+  male = "male",
+  other = "other",
+}
+
+interface IFormInput {
+  firstName: string;
+  gender: GenderEnum;
+}
+
+const TutorialField = () => {
+  const { register, handleSubmit } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("firstName")} />
+        <select {...register("gender")}>
+          <option value="female">female</option>
+          <option value="male">male</option>
+          <option value="other">other</option>
+        </select>
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default TutorialField;
