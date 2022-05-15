db2f92d4d3ef81d8de69742b8306163173679076
n0hack <nohack-@naver.com>
2022-05-16 08:30:55 +0900

    React Hook Form 설정

---
+++ 2022/react-hook-form/tutorial/src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TutorialAlready from "./TutorialAlready";
+import TutorialVerify from "./TutorialVerify";
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <TutorialAlready />
+  </React.StrictMode>
+);
