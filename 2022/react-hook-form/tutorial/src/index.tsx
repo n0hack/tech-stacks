277516a01929181a5ced5a00e83a048752ea7542
n0hack <nohack-@naver.com>
2022-05-16 09:10:21 +0900

    [시작하기] 전역 상태와 사용하기

--- 2022/react-hook-form/tutorial/src/index.tsx
+++ 2022/react-hook-form/tutorial/src/index.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import TutorialUILibrary from "./TutorialUILibrary";
+import { applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./module";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
+import TutorialRedux from "./TutorialRedux";
+
+const store = configureStore({ reducer: rootReducer });
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <TutorialUILibrary />
+    <Provider store={store}>
+      <TutorialRedux />
+    </Provider>
   </React.StrictMode>
 );
