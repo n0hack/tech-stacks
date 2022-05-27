a49a307592586248e3259010a98c085305c6c5d6
n0hack <nohack-@naver.com>
2022-05-27 20:32:11 +0900

    Animation

--- 2022/framer/tutorial/src/App.tsx
+++ 2022/framer/tutorial/src/App.tsx
@@ -1,4 +1,7 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
+
+/* Spring: 현실 세계의 애니메이션 같은 느낌 */
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,18 +11,22 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box
+        transition={{ type: 'spring', delay: 0.5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
     </Wrapper>
   );
 }
