90fc4a3fc690561150535caa138c57e0bb1e6f1a
n0hack <nohack-@naver.com>
2022-05-27 19:55:44 +0900

    framer motion 세팅

---
+++ 2022/framer/tutorial/src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createGlobalStyle } from 'styled-components';
+import App from './App';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: linear-gradient(135deg, #e09, #d0e);
+  }
+`;
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
