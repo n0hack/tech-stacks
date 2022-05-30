956e3d4cf33dab8f4c5741b617122227fa382271
n0hack <nohack-@naver.com>
2022-05-31 00:19:01 +0900

    [netflix] introduction

---
+++ 2022/framer/netflix/src/index.tsx
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import App from './App';
+import { theme } from './theme';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+root.render(
+  <RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </RecoilRoot>
+);
