cfa9f6383794a38f5a2766dfe202071e6230a675
n0hack <nohack-@naver.com>
2022-06-02 18:15:12 +0900

    [feat] Search Redirect

--- 2022/framer/netflix/src/pages/Search.tsx
+++ 2022/framer/netflix/src/pages/Search.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 type Props = {};
 
 export default function Search({}: Props) {
+  const location = useLocation();
+  const [searchParams] = useSearchParams();
+  console.log(location, searchParams.get('keyword'));
   return <div>Search</div>;
 }
