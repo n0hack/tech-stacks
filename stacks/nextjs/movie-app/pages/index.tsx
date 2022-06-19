d98ab3814cad6cbfc3639c303e4e0ee63c642a65
n0hack <nohack-@naver.com>
2022-06-19 19:07:33 +0900

    [feat] 데이터 요청 + redirect/rewrite

--- stacks/nextjs/movie-app/pages/index.tsx
+++ stacks/nextjs/movie-app/pages/index.tsx
@@ -1,10 +1,66 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+interface MovieObject {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
 const Home = () => {
+  const [movies, setMovies] = useState<MovieObject[]>();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch("/api/movies")).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
-      <h1 className="active">Hello</h1>
+      {!movies && <h4>Loading..</h4>}
+      {movies?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            alt="movie poster"
+          />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 };
