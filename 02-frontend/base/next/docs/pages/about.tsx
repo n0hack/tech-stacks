beb1e70067b8c81ae87294d20656e329f02c78ad
n0hack <nohack-@naver.com>
2022-07-15 02:35:06 +0900

    Next.js Serverless

--- 02-frontend/base/next/docs/pages/about.tsx
+++ 02-frontend/base/next/docs/pages/about.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 const About: NextPage = () => {
+  useEffect(() => {
+    const f = async () => {
+      const res = await fetch('/api/hello');
+      const data = await res.json();
+
+      console.log(data);
+    };
+    f();
+  }, []);
+
   return (
     <div>
       About
