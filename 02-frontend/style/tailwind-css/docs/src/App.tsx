aa50446c5779268145bff21055c8af6ec05936e0
n0hack <nohack-@naver.com>
2022-07-16 15:25:16 +0900

    Tailwind CSS 설정 확장

--- 02-frontend/style/tailwind-css/docs/src/App.tsx
+++ 02-frontend/style/tailwind-css/docs/src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <button className="text-green-500 mobile:text-red-400 tablet:text-tor-300">Button 1</button>
+      <button className="bg-black .btn">Button 2</button>
+    </>
+  );
 }
 
 export default App;
--- 02-frontend/style/tailwind-css/docs/tailwind.config.js
+++ 02-frontend/style/tailwind-css/docs/tailwind.config.js
@@ -1,8 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      mobile: { max: "640px" },
+      tablet: { min: "640px", max: "768px" },
+      desktop: { max: "1024px" },
+    },
+    extend: {
+      colors: {
+        tor: {
+          100: "#ffffff",
+          200: "#fffffe",
+          300: "#fffffd",
+        },
+      },
+    },
   },
   plugins: [],
 };
