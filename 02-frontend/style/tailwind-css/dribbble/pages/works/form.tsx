66f8e1a2d80fc1411ea841da1e33c831e6517815
n0hack <nohack-@naver.com>
2022-07-16 20:10:22 +0900

    More Modifiers

--- 02-frontend/style/tailwind-css/dribbble/pages/works/form.tsx
+++ 02-frontend/style/tailwind-css/dribbble/pages/works/form.tsx
@@ -28,6 +28,31 @@ const Form: NextPage = () => {
         </span>
         <input type="submit" value="Login" className="bg-white" />
       </form>
+      <details className="flex flex-col space-y-2 border-t-[1px] mt-5 p-5 select-none open:text-white open:bg-indigo-400">
+        <summary className=" cursor-pointer">What is my fav. food</summary>
+        <span className="">김치</span>
+      </details>
+      <ul className="flex flex-col space-y-2 p-5 mt-5 list-decimal marker:text-teal-500 border-t-[1px] border-dashed border-red-300">
+        <li className="">hi</li>
+        <li className="">hi</li>
+        <li className="">hi</li>
+      </ul>
+      <div className="border-t-[1px] border-dashed border-red-300 mt-5 p-5">
+        <input
+          type="file"
+          className="file:transition-colors file:cursor-pointer file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400 file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file:hover:border"
+        />
+      </div>
+      <div className="border-t-[1px] border-dashed border-red-300 mt-5 p-5">
+        <p className="first-letter:text-7xl">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia
+          error possimus reiciendis perferendis esse quaerat deleniti
+          perspiciatis laborum totam autem, eveniet beatae quidem, ipsum tempore
+          repellat reprehenderit quisquam vel. Dicta ex earum, id sapiente
+          repellendus iure voluptate nulla, porro libero placeat quasi odio
+          accusamus est adipisci deleniti numquam, tempore alias!
+        </p>
+      </div>
     </div>
   );
 };
