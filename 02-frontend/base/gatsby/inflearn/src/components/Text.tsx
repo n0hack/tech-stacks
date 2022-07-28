e0909a5af76cd2feae755af61aed7c3c6d6db0ea
n0hack <nohack-@naver.com>
2022-07-29 00:05:07 +0900

    [chore] Gatsby + Typescript 설정

---
+++ 02-frontend/base/gatsby/inflearn/src/components/Text.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = { text: string };
+
+const Text = ({ text }: Props) => {
+  return <div>{text}</div>;
+};
+
+export default Text;
---
+++ 02-frontend/base/gatsby/inflearn/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Text from "components/Text";
+
+type Props = {};
+
+const IndexPage = ({}: Props) => {
+  return <Text text="Home" />;
+};
+
+export default IndexPage;
